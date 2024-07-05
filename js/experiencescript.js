80901e9eed5cc42abc1ce91f7e2d0f21bb8082e4
clash03 <planbackup14@gmail.com>
2024-07-05 16:55:22 +0800

    First commit

---
+++ js/experiencescript.js
@@ -0,0 +1,7 @@
+function openForm(formId) {
+    document.getElementById(formId).style.display = "block";
+  }
+  
+  function closeForm(formId) {
+    document.getElementById(formId).style.display = "none";
+  }
